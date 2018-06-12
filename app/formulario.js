4f2bc4d25dd3db9653d6de03a67b26aa6c35f96e
peck0 <infpedro@gmail.com>
2018-06-11 23:20:33 -0400

    add guardar en localstorage

--- app/formulario.js
+++ app/formulario.js
@@ -1,34 +1,35 @@
+
+
 function limpiarFormulario(inputs){
     inputs.forEach(function (v, i){
         document.getElementById(v).value ="";        
     });
 }
-
+var guardado = window.localStorage;
 var estudiantes = [];
-
-
 
 function agregarEstudiante(estudiante){
     estudiantes.push(estudiante);
 
     var tablaEstudiante = document.getElementById("tabla_estudiante");
+
     var tdNombre = document.createElement("td");
     var tdMatricula = document.createElement("td");
     var tdIdentificacion = document.createElement("td");
-
     var tr = document.createElement("tr");
+
     tdNombre.textContent = estudiante.nombre;
     tdMatricula.textContent = estudiante.matricula;
     tdIdentificacion.textContent = estudiante.identificacion;
 
+    guardado.setItem("tabla_estudiante", tablaEstudiante);
+    
     tr.appendChild(tdNombre);
     tr.appendChild(tdMatricula);
     tr.appendChild(tdIdentificacion);
 
     tablaEstudiante.appendChild(tr);
-
 }
-
 
 function leerEstudiante(){
     var nombre = document.getElementById("nombre").value;
@@ -44,7 +45,3 @@ function leerEstudiante(){
     console.log(est);
     agregarEstudiante(est);
 }
-
-
-
-
